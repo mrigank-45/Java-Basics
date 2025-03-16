a7dcb63b940ffaa2569d2a99e11e3254427d2bd2
mrigank-45 <mrigankshukla2015@gmail.com>
2025-03-16 23:50:32 +0530

    Equals and Hashcode implimented

--- src/test/java/equals/Car.java
+++ src/test/java/equals/Car.java
@@ -1,5 +1,38 @@
 package test.java.equals;
 
 public class Car {
-    
+
+    private String owner;
+    private String model;
+    private String color;
+
+    public Car(String owner, String model, String color) {
+        this.owner = owner;
+        this.model = model;
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true; // If same object, return true
+        if (obj == null || getClass() != obj.getClass())
+            return false; // Null check & class check
+
+        Car other = (Car) obj; // Typecast to Car
+
+        // Optimized order of comparisons: based on the likelihood of failing
+        return owner.equals(other.owner) &&
+                model.equals(other.model) &&
+                color.equals(other.color);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + owner.hashCode();
+        result = 31 * result + model.hashCode();
+        return 31 * result + color.hashCode();
+    }
+
 }
--- src/test/java/equals/CarTest.java
+++ src/test/java/equals/CarTest.java
@@ -10,7 +10,7 @@ public class CarTest {
     public void primitivesShouldBeEqual() {
         int i = 4;
         int j = 4;
-        assertTrue(i == j);
+        assertTrue(i == j); // True
     }
 
     @Test
@@ -22,8 +22,15 @@ public class CarTest {
         String hello3 = "H";
         hello3 = hello3 + "ello"; // Creates a new String object in heap
         assertTrue(hello1 == hello3); // False, as they are different objects
-        
+
         assertTrue(hello1.equals(hello3));  // True, as it checks character-by-character equality
+    }
+
+    @Test
+    public void porscheShouldBeEqual() {
+        Car myPorsche1 = new Car("Mrigank", "Porsche", "silver");
+        Car myPorsche2 = new Car("Mrigank", "Porsche", "silver");
+        assertTrue(myPorsche1.equals(myPorsche2));  // False if equals() is not overridden
     }
 
 }
