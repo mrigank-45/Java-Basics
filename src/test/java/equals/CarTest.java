b5c2e0cfa8b9c9e80901d6a8eb61f8ab48dfde00
mrigank-45 <mrigankshukla2015@gmail.com>
2025-03-16 23:06:55 +0530

    Introduction to equals method

---
+++ src/test/java/equals/CarTest.java
@@ -0,0 +1,29 @@
+package test.java.equals;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CarTest {
+
+    @Test
+    public void primitivesShouldBeEqual() {
+        int i = 4;
+        int j = 4;
+        assertTrue(i == j);
+    }
+
+    @Test
+    public void stringShouldBeEqual() {
+        String hello1 = "Hello";
+        String hello2 = "Hello";
+        assertTrue(hello1 == hello2); // True, as both refer to the same memory location
+
+        String hello3 = "H";
+        hello3 = hello3 + "ello"; // Creates a new String object in heap
+        assertTrue(hello1 == hello3); // False, as they are different objects
+        
+        assertTrue(hello1.equals(hello3));  // True, as it checks character-by-character equality
+    }
+
+}
